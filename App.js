d999fbc26807edbaea520028788d9a6d284e60ee
BAEK DONGWOO <qorehddn123@naver.com>
2021-07-03 16:19:43 +0900

    notification

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
-import React from 'react';
-
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import messaging from '@react-native-firebase/messaging';
+import {Text, View, Image} from 'react-native';
 
 const App = () => {
+  const [notification, setNotification] = useState({
+    title: undefined,
+    body: undefined,
+    image: undefined,
+  });
+  const getToken = async () => {
+    const token = await messaging().getToken();
+    console.log('.........................................', token);
+  };
+  useEffect(() => {
+    getToken();
+    messaging().onMessage(async remoteMessage => {
+      // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage))
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      setNotification({
+        title: remoteMessage.notification.title,
+        body: remoteMessage.notification.body,
+        image: remoteMessage.notification.android.imageUrl,
+      });
+    });
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log('onNotification Open APP', JSON.stringify(remoteMessage));
+      setNotification({
+        title: remoteMessage.notification.title,
+        body: remoteMessage.notification.body,
+        image: remoteMessage.notification.android.imageUrl,
+      });
+    });
+
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            JSON.stringify(remoteMessage),
+          );
+          setNotification({
+            title: remoteMessage.notification.title,
+            body: remoteMessage.notification.body,
+            image: remoteMessage.notification.android.imageUrl,
+          });
+        }
+      });
+  }, []);
   return (
     <View>
-      <Text>firebase message</Text>
+      <Text>firebase messagetest</Text>
+      <Text>{`title:${notification?.title}`}</Text>
+      <Text>{`body:${notification?.body}`}</Text>
+      <Image source={{uri: notification?.image}} width={500} height={500} />
     </View>
   );
 };
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # React-native-Notification
 React-native Notification
+
+### 1. Install 
+##### Using npm
+npm install --save @react-native-firebase/app
+
+##### Using Yarn
+yarn add @react-native-firebase/app
+
+2. 
+- react-native link @react-native-firebase/app
+
+
+3. 
+- cd android && gradlew signingReport   (windows)
+- cd android && ./gradlew signingReport (mac)
+  
+debugAndroidTest  SHA1
+
+4.
+-  classpath 'com.google.gms:google-services:4.3.8' (/android/build.gradle)
+
+- apply plugin: 'com.google.gms.google-services' // <- Add this line
+  (/android/app/build.gradle)
+  
+5. 
+- npm i @react-native-firebase/messaging
+
+6. 
+- react-native link @react-native-firebase/messaging
